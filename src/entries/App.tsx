6aabb6dfd72fcfdbce84fdb28d253a2b2f568047
chrisdidi <lingmanhau@hotmail.com>
2021-04-15 14:22:33 +0800

    Fix: wrong directory name

--- src/entries/App.tsx
+++ src/entries/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import Menu from "../components/menu/Menu";
+import Menu from "../components/Menu/Menu";
 import ConfigProvider from "../context/configStore";
 import AppRouter from "../routers/app-router";
 import "react-toastify/dist/ReactToastify.css";
